75b39058c0fd69f047ab3fc77adf0579ed18848d
Andrew Kay <warfreak2@gmail.com>
2022-11-26 18:22:43 +0000

    Allow variable tag name to be !DOCTYPE

--- src/compiler/tag.rs
+++ src/compiler/tag.rs
@@ -50,6 +50,8 @@ impl <'a> Compiler<'a> {
                 Some(Value::Str(name)) => {
                     if text::is_identifier(&name) {
                         self.loader.string_pool.insert(&name.to_ascii_lowercase())
+                    } else if name.eq_ignore_ascii_case("!DOCTYPE") {
+                        str_ids::_DOCTYPE
                     } else {
                         self.diagnostics.name_error(NameError::InvalidTag(name), &var.range);
                         str_ids::ANONYMOUS
